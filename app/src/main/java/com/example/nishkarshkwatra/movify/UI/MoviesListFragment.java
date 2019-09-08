668e7bc54076f71b8e401de28e5d8c6612658353
NKwatra <kwatranishkarsh@gmail.com>
2019-09-08 18:28:57 +0530

    Start loading data when fragment is created

--- app/src/main/java/com/example/nishkarshkwatra/movify/UI/MoviesListFragment.java
+++ app/src/main/java/com/example/nishkarshkwatra/movify/UI/MoviesListFragment.java
@@ -4,15 +4,20 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.example.nishkarshkwatra.movify.Networking.MovieDataLoader;
 import com.example.nishkarshkwatra.movify.R;
 
-public class MoviesListFragment extends Fragment {
+public class MoviesListFragment extends Fragment implements LoaderManager.LoaderCallbacks<String> {
 
     // define constants for keys to be used in bundle
     public static final String LOADER_ID_CODE = "loader";
@@ -64,5 +69,29 @@ public class MoviesListFragment extends Fragment {
 
         // Return the root layout of the fragment
         return fragmentRoot;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        getActivity().getSupportLoaderManager().initLoader(mLoaderId, null, this);
+    }
+
+    @NonNull
+    @Override
+    public Loader<String> onCreateLoader(int i, @Nullable Bundle bundle) {
+        return new MovieDataLoader(mPath, getContext(), mMoviesLoadingProgressBar);
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<String> loader, String s) {
+        Log.d("Movies List Fragment", s);
+        mMoviesLoadingProgressBar.setVisibility(View.INVISIBLE);
+        Toast.makeText(getContext(), "Data loading completed", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<String> loader) {
+
     }
 }
