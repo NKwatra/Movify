658e7d674438df4a3254e4a5f1b51286009d9f79
NKwatra <kwatranishkarsh@gmail.com>
2019-09-10 23:16:14 +0530

    Display a list of movie genres in the view pager

--- app/src/main/java/com/example/nishkarshkwatra/movify/UI/GenreListFragment.java
+++ app/src/main/java/com/example/nishkarshkwatra/movify/UI/GenreListFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,11 +12,11 @@ import android.view.ViewGroup;
 import android.widget.ProgressBar;
 
 import com.example.nishkarshkwatra.movify.R;
+import com.example.nishkarshkwatra.movify.adapter.GenreListAdapter;
 
-public class GenreListFragment extends Fragment {
+public class GenreListFragment extends Fragment implements GenreListAdapter.onItemClickListener{
 
     private RecyclerView mGenreListRecyclerView;
-    private ProgressBar mGenreLoadingProgressBar;
 
     public GenreListFragment()
     {
@@ -30,9 +31,17 @@ public class GenreListFragment extends Fragment {
 
         // Get reference to various vies in the layout
         mGenreListRecyclerView = (RecyclerView) fragmentRoot.findViewById(R.id.rv_movies_holder);
-        mGenreLoadingProgressBar = (ProgressBar) fragmentRoot.findViewById(R.id.pb_movies_loading);
+
+        // hook up  recycler view with layout manager and adapter
+        mGenreListRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        mGenreListRecyclerView.setAdapter(new GenreListAdapter(this));
 
         // Return the root layout of the fragment
         return fragmentRoot;
+    }
+
+    @Override
+    public void onItemClick(int id) {
+
     }
 }
---
+++ app/src/main/java/com/example/nishkarshkwatra/movify/adapter/GenreListAdapter.java
@@ -0,0 +1,108 @@
+package com.example.nishkarshkwatra.movify.adapter;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.nishkarshkwatra.movify.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class GenreListAdapter extends RecyclerView.Adapter<GenreListAdapter.GenreHolder> {
+
+    // hash map for genre name to genre id conversion
+    public static HashMap<String, Integer> genreToIdMapping = new HashMap<>();
+
+    // hash map for id to genre conversion
+    public static HashMap<Integer, String> idToGenreMapping;
+
+    static
+    {
+        // define the key value pairs of hash map
+        genreToIdMapping.put("Action", 28);
+        genreToIdMapping.put("Adventure", 12);
+        genreToIdMapping.put("Animation", 16);
+        genreToIdMapping.put("Comedy", 35);
+        genreToIdMapping.put("Crime", 80);
+        genreToIdMapping.put("Documentary", 99);
+        genreToIdMapping.put("Drama", 18);
+        genreToIdMapping.put("Family", 10751);
+        genreToIdMapping.put("Fantasy", 14);
+        genreToIdMapping.put("History", 36);
+        genreToIdMapping.put("Horror", 27);
+        genreToIdMapping.put("Music", 10402);
+        genreToIdMapping.put("Mystery", 9648);
+        genreToIdMapping.put("Romance", 10749);
+        genreToIdMapping.put("Science Fiction", 878);
+        genreToIdMapping.put("TV Movie", 10770);
+        genreToIdMapping.put("Thriller", 53);
+        genreToIdMapping.put("War", 10752);
+        genreToIdMapping.put("Western", 37);
+
+        // create a hash map with keys and values of above interchanged
+        idToGenreMapping = new HashMap<>();
+        for(Map.Entry<String, Integer> entry: genreToIdMapping.entrySet())
+        {
+            idToGenreMapping.put(entry.getValue(), entry.getKey());
+        }
+    };
+
+    // interface for handling click events
+    public interface onItemClickListener
+    {
+        void onItemClick(int id);
+    }
+
+    // data source for adapter, a list of genres
+    private ArrayList<String> mGenreDataset;
+
+    // reference to click listener
+    private onItemClickListener mClickListener;
+
+    // constructor
+    public GenreListAdapter(onItemClickListener listener)
+    {
+        mClickListener = listener;
+        mGenreDataset = new ArrayList<>(genreToIdMapping.keySet());
+    }
+
+    // method to supply view holder
+    @NonNull
+    @Override
+    public GenreHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.genre_list_item, viewGroup, false);
+        return new GenreHolder(view);
+    }
+
+    // method to bind view holder with data
+    @Override
+    public void onBindViewHolder(@NonNull GenreHolder genreHolder, int i) {
+        genreHolder.mGenreNameTextView.setText(mGenreDataset.get(i));
+    }
+
+    // total number of items
+    @Override
+    public int getItemCount() {
+        return mGenreDataset.size();
+    }
+
+    // View holder class
+    public class GenreHolder extends RecyclerView.ViewHolder
+    {
+        // reference to cache view ids
+        private TextView mGenreNameTextView;
+
+        public GenreHolder(View view)
+        {
+            super(view);
+            mGenreNameTextView = (TextView) view.findViewById(R.id.tv_genre_name);
+        }
+    }
+
+
+}
